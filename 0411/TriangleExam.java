41cddb449c2c5fcf9527564a5ccbf3b3daa8413b
Jsh031006 <102495150+Jsh031006@users.noreply.github.com>
2022-04-25 10:43:12 +0900

    Add files via upload

---
+++ 0411/TriangleExam.java
@@ -0,0 +1,20 @@
+public class TriangleExam {
+	public static void main(String[] args) {
+		Triangle tr = new Triangle(10.2, )
+	}
+}
+class Triangle{
+	double bottom;
+	double height;
+	public Triangle(double bt, double hg) {
+		this.bottom = bt;
+		this.height = hg;
+	}
+public void setSize(double bt, double hg) {
+		this.bottom = bt;
+		this.height = hg;
+	}
+public double getArea() {
+	return bottom*height/2;
+}
+}
